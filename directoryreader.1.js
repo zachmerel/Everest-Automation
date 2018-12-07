bd5c50de0c2a6493221c54696036d17a76cbdf1b
zmerel123 <32908426+zachmerel@users.noreply.github.com>
2018-12-07 15:22:24 -0600

    gets file removes extension
    with comments, need to remove duplicates

---
+++ directoryreader.1.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+//declares the folder where the files are being read
+ let directory = "C:/Automation/test_invoices";
+//reads the filenames in the folder
+ let files = fs.readdirSync(directory);
+// console.log(files);
+//for loop cycles through the filenames read
+for(i=0; i< files.length; i++){
+    //removes the extension from file name but leaves page indicator (3 or 5)
+    // let extfree = files[i].substring(0, files[i].lastIndexOf("."));
+    //removes the extension from file name and page indicator (3 or 5)
+    let extfree = files[i].substring(0, 6);
+    //creates array to put edited filenames in
+    let noExtensions = []
+    //pushes edited file names to array
+    noExtensions.push(extfree);
+
+    
+    
+
+   
+
+    console.log(noExtensions);
+    
+};
