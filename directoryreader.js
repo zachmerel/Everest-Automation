027087bf430313220332aef7932b0ca48c46a4a8
zach merel <zachmerel@gmail.com>
2018-12-13 10:04:34 -0600

    removed duplicates from array

--- directoryreader.js
+++ directoryreader.js
@@ -1,12 +1,25 @@
 const fs = require("fs");
 
+const loDash = require("lodash");
 
- let directory = "C:/Automation/test_invoices";
-
+//declares the folder where the files are being read
+ let directory = "test_invoices";
+//reads the filenames in the folder
  let files = fs.readdirSync(directory);
-console.log(files);
-
+ //creates array to put edited filenames in
+ let noExtensions = []
+//for loop cycles through the filenames read
 for(i=0; i< files.length; i++){
-    let noExtensions = files[i].substring(0, files[i].lastIndexOf("."));
-    console.log(noExtensions);
+    //removes the extension from file name but leaves page indicator (3 or 5)
+    // let extfree = files[i].substring(0, files[i].lastIndexOf("."));
+    //removes the extension from file name and page indicator (3 or 5)
+    let extfree = files[i].substring(0, 6);
+    
+    
+    //pushes edited file names to noExtensions array
+    noExtensions.push(extfree);
+    // console.log(noExtensions);
 };
+
+let duplicateFree = loDash.sortedUniq(noExtensions);
+console.log(duplicateFree)
