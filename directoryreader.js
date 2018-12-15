2dfb41c31d7d8893f1c37c0734f936bf47b7614a
Zach Merel <zachmerel@gmail.com>
2018-12-15 10:08:09 -0600

    output excel formatted array?

--- directoryreader.js
+++ directoryreader.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 
 const loDash = require("lodash");
 
-// const xlsx = require("xlsx");
+const xlsx = require("xlsx");
 //declares the folder where the files are being read
- let directory = "test_invoices";
+ let directory = "temp";
 //reads the filenames in the folder
  let files = fs.readdirSync(directory);
  //creates array to put edited filenames in
@@ -23,10 +23,17 @@ for(i=0; i< files.length; i++){
 };
 
 let duplicateFree = loDash.sortedUniq(noExtensions);
-console.log(duplicateFree)
+console.log(duplicateFree);
+var ws = xlsx.utils.aoa_to_sheet([
+    duplicateFree,
+    [1,2,3,4,5,6,7],
+    [2,3,4,5,6,7,8]
+  ]);
+console.log(ws);
+// XLSX.utils.sheet_add_aoa(ws, [[1,2], [2,3], [3,4]], {origin: "A2"});
 //function to write to batches workbook?
-XLSX.writeFile(wb, "Batches-Sent-to-RTS-List", write_opts);
-//link to sheetjs docs
-//https://sheetjs.gitbooks.io/docs/demos/demos/#interface
-//this will import an array of array to a certain area of a work sheet
+// XLSX.writeFile(wb, "Batches-Sent-to-RTS-List", write_opts);
+// link to sheetjs docs
+// https://sheetjs.gitbooks.io/docs/demos/demos/#interface
+// this will import an array of array to a certain area of a work sheet
 // XLSX.utils.sheet_add_aoa(<----this is where the batches sheet path goes?---->, duplicateFree, {origin: "I6"});
