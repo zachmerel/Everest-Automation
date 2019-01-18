97053404502a14d5e65d6ddbddedebd8e84d5759
zach merel <zachmerel@gmail.com>
2019-01-18 14:29:06 -0600

    merges 2 files with same invoice number. does not assign to directory and hasn't been tested with 3 yet.

--- pdfmerge.js
+++ pdfmerge.js
@@ -1,14 +1,14 @@
 //packages required
-var merge = require('easy-pdf-merge');
+const merge = require('easy-pdf-merge');
+const path = require('path');
 const fs = require("fs");
+const fse = require('fs-extra');
 const loDash = require("lodash");
 
 //declaring files that will be merged
 let file1 = "";
 let file2 = "";
-let filename ="";
-
-// let matchingFiles = [];
+let filename = "";
 
 //declaring the directory that will be read and looped through
 const directory = "temp v1/";
@@ -21,21 +21,19 @@ for (i = 0; i < files.length; i++) {
     console.log("file1", file1);
     file2 = (`${directory}${files[i + 1]}`);
     console.log("file2", file2);
-    break;
-}
 
-console.log("file1 after for loop", file1);
-console.log("file2 after for loop", file2);
-console.log("filename",filename);
-//if first 6 characters match in files 1 and 2 ...
-if ((file1.substring(0, 6)) === (file2.substring(0, 6))) {
-    //merges two files and renames them
-    merge([file1, file2], (`${filename}.pdf`), (err) => {
-
-        if (err)
-            return console.log(err);
-
-        console.log('Successfully merged!');
-
-    });
+    //if first 6 characters match in files 1 and 2 ...
+    if ((file1.substring(0, 6)) === (file2.substring(0, 6))) {
+        //merges two files and renames them
+        merge([file1, file2], (`${filename}.pdf`), (err) => {
+            if (err)
+                return console.log(err);
+            console.log('Successfully merged!');
+            //append content at the end of the file:
+            // fs.appendFile((`${filename}.pdf`),'testDestination',  function (err) {
+            //     if (err) throw err;
+            //     console.log('Updated!');
+            // });
+        });
+    }
 };
