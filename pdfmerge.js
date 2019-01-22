0769195392328c07b5ea0e9d234d2e52981a290e
zach merel <zachmerel@gmail.com>
2019-01-22 12:40:02 -0600

    added script commands

--- pdfmerge.js
+++ pdfmerge.js
@@ -17,18 +17,21 @@ let files = fs.readdirSync(directory);
 
 for (i = 0; i < files.length; i++) {
     filename = (files[i].substring(0, 6))
+    console.log("filename",filename);
     file1 = (`${directory}${files[i]}`);
-    console.log("file1", file1);
+    // console.log("file1", file1);
     file2 = (`${directory}${files[i + 1]}`);
-    console.log("file2", file2);
-
-    //if first 6 characters match in files 1 and 2 ...
+    // console.log("file2", file2);
+    // break;
+    // if first 6 characters match in files 1 and 2 ...
     if ((file1.substring(0, 6)) === (file2.substring(0, 6))) {
         //merges two files and renames them
+        console.log("filename after if ",filename);
         merge([file2, file1], (`${filename}.pdf`), (err) => {
             if (err)
                 return console.log(err);
             console.log('Successfully merged!');
+            console.log("filename after merge ",filename);
             //append content at the end of the file:
             // fs.appendFile((`${filename}.pdf`),'testDestination',  function (err) {
             //     if (err) throw err;
