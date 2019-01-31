65c2961963db35da39d6df0233d9cfc463517694
zach merel <zachmerel@gmail.com>
2019-01-31 15:11:11 -0600

    reading the email attachment names from the macros extraction and getting just the file name and pushing to new Array

--- pdfmerge2.js
+++ pdfmerge2.js
@@ -1,8 +1,3 @@
-//NOTES
-//create an array of array. search the directory any files that have the same first 6 didgits push them to an array. 
-//need to .chunk the array for every set of duplicates
-//loop through the array of arrays and merge each array into its own pdf using easy pdf merge.
-
 //packages required
 const merge = require('easy-pdf-merge');
 const path = require('path');
@@ -13,7 +8,7 @@ const loDash = require("lodash");
 
 
 //declaring the directory that will be read and looped through
-const directory = "temp 1-2/";
+const directory = "K:/MEGA/Daily Request - 30D/";
 //reads the filenames in the folder
 let files = fs.readdirSync(directory);
 //creates array to put edited filenames in
@@ -23,7 +18,7 @@ for (i = 0; i < files.length; i++) {
     let first6 = files[i].substring(0, 6);
     first6Array.push(first6);
 };
-console.log(first6Array);
+// console.log(first6Array);
 //declares masterArray
 const masterArray = [];
 
@@ -32,7 +27,7 @@ const unique = new Set(first6Array);
 unique.forEach(u => {
     masterArray.push(first6Array.filter(e => e === u));
 });
-console.log(masterArray);
+// console.log(masterArray);
 
 //loops through masterArray to see how many files are in each index. If 3 assign 3,4,5 and directory name , if 2 assign 3,5 and directory name.
 for (i = 0; i < masterArray.length; i++) {
