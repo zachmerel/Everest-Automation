910ce733d69f72d001836c25b2d5d1f982b6dc6a
zach merel <zachmerel@gmail.com>
2019-02-12 15:22:50 -0600

    went backt to example from docs to start over from beginning

--- pdf2Json.js
+++ pdf2Json.js
@@ -1,16 +1,11 @@
-const fs = require('fs'),
+let fs = require('fs'),
 PDFParser = require("pdf2json");
 
-// function loadPDF(pdfFilePath)
-let filePath = "Everest-Automation/temptest1/140654-4.pdf"
-let pdfParser = new PDFParser(filePath);
-console.log(pdfParser)
+let pdfParser = new PDFParser();
 
 pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
 pdfParser.on("pdfParser_dataReady", pdfData => {
-fs.writeFile("Everest-Automation.json", JSON.stringify(pdfData));
+fs.writeFile("Everest-Automation/temptest1/140654-4.json", JSON.stringify(pdfData));
 });
 
-
-let pdf = pdfParser.loadPDF("Everest-Automation/temptest1/140654-4.pdf");
-console.log(pdf)
+pdfParser.loadPDF("Everest-Automation/temptest1/140654-4.pdf");
