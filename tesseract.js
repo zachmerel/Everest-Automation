5095169f3925d7b8821cf702af6b256ccd817da4
zach merel <zachmerel@gmail.com>
2019-02-14 15:11:51 -0600

    trying to figure a way to identify the reference number from the rest of the 8 didgit segments of text from the pdf

--- tesseract.js
+++ tesseract.js
@@ -1,9 +1,20 @@
 const Tesseract = require('tesseract.js')
 
 let myImage = "141848_4.png"
-
+let testArray = [];
+let referenceNum = [];
 
 Tesseract.recognize(myImage)
-.then(function(result){
-    console.log(result.text)
-})
+    .then(function (result) {
+        console.log( typeof (result.text))
+        // testArray.push(result.text)
+        let split = (result.text).split(" ")
+        for(i=0; i < split.length; i++){
+            // console.log("splitpiece",split[i])
+            if(split[i].length == 8){
+                console.log(split[i])
+            }
+        }
+        // console.log("split[0]",split[0])
+    })
+
