88d271222bc8ca8a0515413c6704464d29603c85
zach merel <zachmerel@gmail.com>
2019-02-15 08:55:09 -0600

    will parse from an array, still need to match reference number by load number in the console.

--- tesseract.js
+++ tesseract.js
@@ -1,21 +1,18 @@
 const Tesseract = require('tesseract.js')
 
-let myImage = "141848_4.png"
-let testArray = [];
-let referenceNum = [];
+let myImage = ["./temp v1/143559 3_Page_2.png", "141848_4.png"]
 
-Tesseract.recognize(myImage)
-    .then(function (result) {
-        console.log( typeof (result.text))
-        console.log(result.text)
-        // testArray.push(result.text)
-        let split = (result.text).split(" ")
-        for(i=0; i < split.length; i++){
-            // console.log("splitpiece",split[i])
-            if(split[i].length == 8){
-                console.log(split[i])
+for (i = 0; i < myImage.length; i++) {
+    Tesseract.recognize(myImage[i])
+        .then(function (result) {
+            // console.log( typeof (result.text))
+            // console.log(result.text)
+            let split = (result.text).split(" ")
+            for (i = 0; i < split.length; i++) {
+                if (split[i].length == 8 && !isNaN(split[i])) {
+                    console.log(split[i])
+                }
             }
-        }
-        // console.log("split[i]",split)
-    })
-
+            // console.log("split[i]",split)
+        })
+};
