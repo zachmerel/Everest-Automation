9767607db456a4af2323c726660d7244eb7c5270
zach merel <zachmerel@gmail.com>
2019-02-19 10:39:57 -0600

    tesseract will loop through the directory of pdf/ converted pngs and push all the pngs into an array ready to be parsed through.

--- pdf2png.js
+++ pdf2png.js
@@ -1,12 +1,15 @@
+//dependencies
 const path = require("path");
 const pdf = require("pdf-poppler");
 const fs = require("fs");
 
+//directory of pdfs
 let directory = "invoiceMergeTest";
 
+//reads directory
 let files = fs.readdirSync(directory);
 
-// const findFile = `./${directory}/${files[i]}`
+//loops through directory and reassigns files names and paths
 for (i = 0; i < files.length; i++) {
     console.log(files[i])
   let opts = {
@@ -15,7 +18,7 @@ for (i = 0; i < files.length; i++) {
     out_prefix: path.basename(`./${directory}/${files[i]}`, path.extname(`./${directory}/${files[i]}`)),
     page: null
   };
-
+// converts pdfs to pngs
   pdf
     .convert(`./${directory}/${files[i]}`, opts)
     .then(res => {
@@ -26,19 +29,4 @@ for (i = 0; i < files.length; i++) {
     });
 }
 
-//  let testFile = "./invoiceMergeTest/143700_3.pdf"
-// let opts = {
-//     format: "png",
-//     out_dir: path.dirname(testFile),
-//     out_prefix: path.basename(testFile, path.extname(testFile)),
-//     page: null
-//   };
 
-//   pdf
-//     .convert(testFile, opts)
-//     .then(res => {
-//       console.log("Successfully converted");
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
--- tesseract.js
+++ tesseract.js
@@ -1,24 +1,49 @@
+//dependencies
 const Tesseract = require('tesseract.js')
+const fs = require("fs");
+const fileExtension = require('file-extension');
 
+//directory of invoices 
+let directory = "invoiceMergeTest";
 
-let myImage = ["./temp v1/143559 3_Page_2.png", "141848_4.png"]
-let referenceNumbers = [];
+// //array of pngs to be parsed
+let myImage = []
+//reads through directory 
+let files = fs.readdirSync(directory);
 
-for (i = 0; i < myImage.length; i++) {
-    Tesseract.recognize(myImage[i])
-        .then(function (result) {
-            // console.log( typeof (result.text))
-            // console.log(result.text)
-            let split = (result.text).split(" ")
-            for (i = 0; i < split.length; i++) {
-                if (split[i].length == 8 && !isNaN(split[i])) {
-                    referenceNumbers.push(split[i])
-                    
-                }
-            }
-            console.log(referenceNumbers)
-            // console.log("split[i]",split)
-        })
+//loops through directory to find all pngs and push them to myImage array.
+for (i = 0; i < files.length; i++) {
+    let extType = fileExtension(files[i])
+    console.log("extTYpe:",extType);
+    console.log(files[i])
+    if(extType === "png"){
+        myImage.push(files[i])
+    }
 };
+
+
+console.log("myImage", myImage)
+// //array of reference numbers that need to be checked
+// let referenceNumbers = [];
+// //loops through myImage array and parses png to txt files
+// for (i = 0; i < myImage.length; i++) {
+//     Tesseract.recognize(myImage[i])
+//         .then(function (result) {
+//             // console.log( typeof (result.text))
+//             // console.log(result.text)
+//             //splits string output of tesseract into an array
+//             let split = (result.text).split(" ")
+//             //loops through array to pull out reference numbers
+//             for (i = 0; i < split.length; i++) {
+//                 if (split[i].length == 8 && !isNaN(split[i])) {
+//                     referenceNumbers.push(split[i])
+                    
+//                 }
+//             }
+//             //logs reference numbers found
+//             console.log(referenceNumbers)
+//             // console.log("split[i]",split)
+//         })
+// };
 
 // process.exit()
