3459497d5329eb1733ee1bfe394d05b0c79818ce
zach merel <zachmerel@gmail.com>
2019-02-19 11:53:00 -0600

    tesseract doesn't parse through png and capture all of the text, testing if the png convert is the cause of this.

--- pdf2Json.js
+++ pdf2Json.js
@@ -1,11 +1,25 @@
-let fs = require('fs'),
-PDFParser = require("pdf2json");
+// let fs = require('fs'),
+// PDFParser = require("pdf2json");
 
-let pdfParser = new PDFParser();
+// let pdfParser = new PDFParser();
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-fs.writeFile("Everest-Automation/temptest1/140654-4.json", JSON.stringify(pdfData));
-});
+// pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+// pdfParser.on("pdfParser_dataReady", pdfData => {
+// fs.writeFile("Everest-Automation/temptest1/140654-4.json", JSON.stringify(pdfData));
+// });
 
-pdfParser.loadPDF("Everest-Automation/temptest1/140654-4.pdf");
+// pdfParser.loadPDF("Everest-Automation/temptest1/140654-4.pdf");
+ isInt = (n) => {
+    return n % 1 === 0;
+ }
+
+var num = 25121.57  
+var typeOf = (typeof(num))
+console.log(typeOf)
+
+if(!isNaN(num) && isInt(num)) {
+    console.log("true")
+}
+else{
+    console.log("false")
+}
--- tesseract.js
+++ tesseract.js
@@ -11,11 +11,16 @@ let myImage = []
 //reads through directory 
 let files = fs.readdirSync(directory);
 
+//function that determines if 8 didgit number is an integer (making it a true reference number)
+ isInt = (n) => {
+    return n % 1 === 0;
+ }
+
 //loops through directory to find all pngs and push them to myImage array.
 for (i = 0; i < files.length; i++) {
     let extType = fileExtension(files[i])
-    console.log("extTYpe:",extType);
-    console.log(files[i])
+    // console.log("extTYpe:",extType);
+    // console.log(files[i])
     if(extType === "png"){
         myImage.push(files[i])
     }
@@ -23,27 +28,29 @@ for (i = 0; i < files.length; i++) {
 
 
 console.log("myImage", myImage)
-// //array of reference numbers that need to be checked
-// let referenceNumbers = [];
-// //loops through myImage array and parses png to txt files
-// for (i = 0; i < myImage.length; i++) {
-//     Tesseract.recognize(myImage[i])
-//         .then(function (result) {
-//             // console.log( typeof (result.text))
-//             // console.log(result.text)
-//             //splits string output of tesseract into an array
-//             let split = (result.text).split(" ")
-//             //loops through array to pull out reference numbers
-//             for (i = 0; i < split.length; i++) {
-//                 if (split[i].length == 8 && !isNaN(split[i])) {
-//                     referenceNumbers.push(split[i])
+//array of reference numbers that need to be checked
+let referenceNumbers = [];
+//loops through myImage array and parses png to txt files
+for (i = 0; i < myImage.length; i++) {
+    Tesseract.recognize(`./${directory}/${myImage[i]}`)
+        .then(function (result) {
+            // console.log( typeof (result.text))
+            console.log(result.text)
+            //splits string output of tesseract into an array
+            let split = (result.text).split(" ")
+            console.log("split",split)
+            //loops through array to pull out reference numbers
+            for (i = 0; i < split.length; i++) {
+                if (split[i].length == 8 && isInt(split[i])) {
+                    referenceNumbers.push(split[i])
                     
-//                 }
-//             }
-//             //logs reference numbers found
-//             console.log(referenceNumbers)
-//             // console.log("split[i]",split)
-//         })
-// };
+                }
+            }
+            console.log("finished one doc")
+            //logs reference numbers found
+            console.log(referenceNumbers)
+            // console.log("split[i]",split)
+        })
+};
 
 // process.exit()
