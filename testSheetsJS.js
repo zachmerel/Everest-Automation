3444ab5ac0bf4c41cff1c473030386b592344b2b
zach merel <zachmerel@gmail.com>
2019-02-22 15:02:32 -0600

    look at testSheetJS file comment for possible soluton to line break issue.

--- testSheetsJS.js
+++ testSheetsJS.js
@@ -1,4 +1,10 @@
 var x = '24176801\nCommodity';
-for(i=0;i<x.length;i++){
-    x[i].split(\n)
-}
+console.log(x)
+if (x.includes("\n")) {
+    console.log("contains line break")
+       var test = x.split("\n");
+       console.log(test)
+    }
+// }
+
+//need to push the new split array into new array if does not contain line break then push to the new array.
