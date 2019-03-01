ce7837d30376468d6e1b62010812e6af2ecfc2db
zach merel <zachmerel@gmail.com>
2019-03-01 15:17:02 -0600

    added some sudo code still can't enter credentials after alert is closed

--- seleniumTest.js
+++ seleniumTest.js
@@ -19,11 +19,38 @@ const enterPassword = () => {
     driver.findElement(By.name('cmdSubmit')).sendKeys(Keys.ENTER);
 }
 
+const closeAlert = () => {
+    driver.wait(until.alertIsPresent()).then(() => { driver.switchTo().alert().accept(); });
+}
+
 driver.get('https://www.budexchange.com/dana-na/auth/url_17/welcome.cgi');
-driver.wait(until.alertIsPresent()).then(() => { driver.switchTo().alert().accept(); });
-driver.sleep(5000)
-enterPassword();
-// driver.navigate.windows(windowHandle)
+closeAlert();
+
+
+
+//Open portal Promise trying to make a promise so that once the alert has been closed I'm able to enter in the password and not before then.
+const openPortal = new Promise(
+    function (resolve, reject) {
+        if(//need a call back to the close alert function to make sure its been executed?//){
+
+        }else{
+            console.log("not working")
+        }
+
+
+        );
+// driver.wait(until.alertIsPresent()).then(() => { driver.switchTo().alert().accept(); });
+// console.log("this is the alert getting reconized and closed")
+    
+// driver.wait(until.alertIsPresent(1)).then(() => enterPassword());
+// console.log("this is when I try to pass the username and password in and press enter")
+
+
+
+
+
+// driver.sleep(5000)
+// enterPassword();
 // driver.findElement(By.name('username')).sendKeys('je773425');
 // driver.findElement(By.name('password')).sendKeys('Vologda85');
 // driver.findElement(By.name('cmdSubmit')).sendKeys(Keys.ENTER);
