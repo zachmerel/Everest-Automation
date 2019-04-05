586a94cf3c5a936e0de749174dfcd3e57eab6bd6
zach merel <zachmerel@gmail.com>
2019-04-05 15:13:41 -0500

    scoping issue and other issue with passing an array into a for loop inside an async funciton

--- seleniumTest.js
+++ seleniumTest.js
@@ -1,8 +1,8 @@
 // dependencies
 const webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    Keys = webdriver.Key,
-    until = webdriver.until;
+  By = webdriver.By,
+  Keys = webdriver.Key,
+  until = webdriver.until;
 const chrome = require('selenium-webdriver/chrome');
 const path = require('chromedriver').path;
 
@@ -12,25 +12,31 @@ const service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 
 //allows the use of webdriver
-var driver = new webdriver.Builder()
-    .withCapabilities(webdriver.Capabilities.chrome())
-    .build();
+const driver = new webdriver.Builder()
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
 
 
-//enters username, password, hits enter
-const enterPassword = async () => {
-    console.log("this is where the password is entered")
-    await driver.findElement(By.name('username')).sendKeys('je773425');
-    await driver.findElement(By.name('password')).sendKeys('Everest2019');
-    await driver.findElement(By.name('cmdSubmit')).sendKeys(Keys.ENTER);
+
+//opens the portal initially
+const openPortal = async () => {
+  await driver.get('https://www.budexchange.com/dana-na/auth/url_17/welcome.cgi');
 }
 
 //switches window to alert and accepts it
 const closeAlert = async () => {
-    console.log("this is where the alert gets closed")
-    await driver.wait(until.alertIsPresent())
-    await driver.switchTo().alert().accept();
+  console.log("this is where the alert gets closed")
+  await driver.wait(until.alertIsPresent())
+  await driver.switchTo().alert().accept();
 
+}
+
+//enters username, password, hits enter
+const enterPassword = async () => {
+  console.log("this is where the password is entered")
+  await driver.findElement(By.name('username')).sendKeys('je773425');
+  await driver.findElement(By.name('password')).sendKeys('Everest2019');
+  await driver.findElement(By.name('cmdSubmit')).sendKeys(Keys.ENTER);
 }
 
 //gets to the carrier communcation page
@@ -39,32 +45,35 @@ const carrierCommunication = () => {
 }
 
 //enters in the reference number for the load
-const referenceInput = async () => {
- await driver.findElement(By.name('txtSearchBy')).sendKeys('24343767')
- await driver.findElement(By.name('searchButton')).sendKeys(Keys.ENTER);
- await driver.findElement(By.name('rptBalDues:_ctl1:btnShowDetails')).sendKeys(Keys.ENTER);
-
+const referenceInput = async (testArray) => { 
+  //test array to figure out looping through reference numbers
+ testArray = ['24342989', '304731', '24343767']
+testArray.array.forEach(async(element) => {
+  
+});
+  await func(driver.findElement(By.name('txtSearchBy')).sendKeys(testArray[i]))
+  await driver.findElement(By.name('searchButton')).sendKeys(Keys.ENTER);
+  await driver.findElement(By.name('rptBalDues:_ctl1:btnShowDetails')).sendKeys(Keys.ENTER);
+ }
 }
-//opens the portal initially
-const openPortal = async () => {
-   await driver.get('https://www.budexchange.com/dana-na/auth/url_17/welcome.cgi');
-    // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-}
 
+//async function that opens that waits for the login function
 const app = async () => {
   await login();
   console.log('done');
 }
 
-async function login() {
-  await closeAlert();
-  await enterPassword();
-  await carrierCommunication();
-  await referenceInput();
-}
-
+// for (i = 0; i < testArray.length; i++) {
+  async function login(testArray) {
+    await closeAlert();
+    await enterPassword();
+    await carrierCommunication();
+    await referenceInput();
+  }
+// }
+//calls the open portal function
 openPortal();
 
 Promise.resolve(app()).then(() => {
-  
+
 })
